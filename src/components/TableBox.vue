81afd9da14fc542aaa409c2a614816a3719a90a3
inubayuaji <inubayuaji@gmail.com>
2020-02-23 20:29:30 +0700

    change to interactjs

--- src/components/TableBox.vue
+++ src/components/TableBox.vue
@@ -1,5 +1,5 @@
 <template>
-  <Drag class="card" v-bind:id="id" :drag-handle="'.card-header'">
+  <div class="card" v-bind:id="id" ref="tableBox">
     <div class="card-header">
       New entity {{ id }}
       <button v-on:click="addRow(id)">+</button>
@@ -16,11 +16,11 @@
         ></Row>
       </ul>
     </div>
-  </Drag>
+  </div>
 </template>
 
 <script>
-import Drag from 'vue-draggable-resizable'
+import interact from 'interactjs'
 import Row from './Row'
 import store from '../store/index'
 
@@ -33,6 +33,27 @@ export default {
     }
   },
   methods: {
+    initInteract: function (el) {
+      interact(el).draggable({
+        onmove: this.dragMoveListener,
+        allowFrom: '.card-header'
+      })
+    },
+    dragMoveListener: function (event) {
+      var target = event.target
+
+      // keep the dragged position in the data-x/data-y attributes
+      var x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx
+      var y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy
+
+      // translate the element
+      target.style.left = x + 'px'
+      target.style.top = y + 'px'
+
+      // update the posiion attributes
+      target.setAttribute('data-x', x)
+      target.setAttribute('data-y', y)
+    },
     delTable: function (id) {
       store.commit('delTable', id)
     },
@@ -40,12 +61,24 @@ export default {
       store.commit('addRow', id)
     }
   },
+  mounted: function () {
+    var el = this.$refs.tableBox
+    this.initInteract(el)
+  },
   props: {
     id: String
   },
   components: {
-    Drag,
     Row
   }
 }
 </script>
+
+<style scoped>
+.card{
+  width: 300px;
+  position: absolute;
+  touch-action: none;
+  user-select: none;
+}
+</style>
